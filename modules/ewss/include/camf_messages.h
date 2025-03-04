43063bdc6a858829464b35e31b1a82b5de13f539
Didier DONSEZ <didier.donsez@gmail.com>
2025-03-04 16:10:14 +0100

    add global header

---
+++ modules/ewss/include/camf_messages.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2020-2025 Université Grenoble Alpes
+ */
+
+/*
+ * Author : Didier Donsez, Université Grenoble Alpes
+ */
+
+ #ifndef _CAMF_MESSAGES_H_
+ #define _CAMF_MESSAGES_H_
+
+#include "build/include/values/camf_18_3_1.h"
+#include "build/include/values/camf_18_3_2.h"
+#include "build/include/values/camf_18_3_3.h"
+
+// TODO add language dependent messages
+
+ #endif
