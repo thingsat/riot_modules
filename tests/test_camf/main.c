2075cd0d65466093c23743a58b9e03371580a63b
Didier DONSEZ <didier.donsez@gmail.com>
2025-03-05 18:09:22 +0100

    printf CAMF messages

--- tests/test_camf/main.c
+++ tests/test_camf/main.c
@@ -16,20 +16,30 @@
 
  #include <stdio.h>
  #include <stdint.h>
- #include <string.h>
  #include <stdlib.h>
  
  // #include <shell.h>
  #include "camf_payload.h"
+ #include "camf_utils.h"
  
-static const uint8_t camf_message_test_1_ba[16] = {
-     0b00000000, 0b00000000, 0b00000000, 0b00000000,
-     0b00000000, 0b00000000, 0b00000000, 0b00000000,
-     0b00000000, 0b00000000, 0b00000000, 0b00000000,
-     0b00000000, 0b00000000, 0b00000000, 0b00000000,
- };
 
- static const CommonAlertMessageFormat_t* camf_message_test_1 = (const CommonAlertMessageFormat_t*)camf_message_test_1_ba;
+ static const uint8_t camf_message_test_0_ba[16] = {
+    0b00000000, 0b00000000, 0b00000000, 0b00000000,
+    0b00000000, 0b00000000, 0b00000000, 0b00000000,
+    0b00000000, 0b00000000, 0b00000000, 0b00000000,
+    0b00000000, 0b00000000, 0b00000000, 0b00000000,
+};
+
+static const CommonAlertMessageFormat_t* camf_message_test_0 = (const CommonAlertMessageFormat_t*)camf_message_test_0_ba;
+
+static const uint8_t camf_message_test_1_ba[16] = {
+    0b00000000, 0b00000000, 0b00000000, 0b00000000,
+    0b00000000, 0b00000000, 0b00000000, 0b00000000,
+    0b00000000, 0b00000000, 0b00000000, 0b00000000,
+    0b00000000, 0b00000000, 0b00000000, 0b00000000,
+};
+
+static const CommonAlertMessageFormat_t* camf_message_test_1 = (const CommonAlertMessageFormat_t*)camf_message_test_1_ba;
 
 
  /* main */
@@ -37,7 +47,8 @@ static const uint8_t camf_message_test_1_ba[16] = {
  {
     printf("\n************ CAMF test program ***********\n");
  
-    (void) camf_message_test_1;
+    camf_message_printf(camf_message_test_0);
+    camf_message_printf(camf_message_test_1);
 
      /* run the shell */
  //    char line_buf[SHELL_DEFAULT_BUFSIZE];
@@ -45,4 +56,3 @@ static const uint8_t camf_message_test_1_ba[16] = {
  
      return 0;
  }
- 
