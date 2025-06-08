91f95082cc4a8be659d402a6d35d67119437a303
Didier DONSEZ <didier.donsez@gmail.com>
2025-06-08 13:03:44 +0200

    enable GPS PPS into SX1302

--- apps/basic_mission/Makefile.gnss.mk
+++ apps/basic_mission/Makefile.gnss.mk
@@ -27,12 +27,16 @@ ifeq ($(BOARD),thingsat-up4)
 # Set UART board for GNSS
 GPS_UART_DEV = 1
 GPS_UART_BAUDRATE ?= 9600
+# Disable GPS in SX1302 driver
+ENABLE_GPS = 0
 endif
 
 ifeq ($(BOARD),thingsat-up1-f4)
 # Set UART board for GNSS
 GPS_UART_DEV = 1
 GPS_UART_BAUDRATE ?= 9600
+# Disable GPS in SX1302 driver
+ENABLE_GPS = 0
 endif
 
 ifeq ($(RAK5146_ON_NUCLEO),yes)
@@ -46,6 +50,8 @@ GPS_BAUDRATE = 9600
 GPS_UART_DEV = 1
 GPS_RESET_PIN = ARDUINO_PIN_6
 GPS_UART_ENABLE_TRACE = 0
+# Enable GPS in SX1302 driver
+ENABLE_GPS = 1
 endif
 
 
@@ -64,6 +70,8 @@ GPS_UART_DEV = 1
 GPS_RESET_PIN = GPIO_PIN\(PORT_A,8\)
 GPS_STANDBY_PIN = GPIO_PIN\(PORT_A,15\)
 GPS_UART_ENABLE_TRACE = 0
+# Enable GPS in SX1302 driver
+ENABLE_GPS = 1
 endif
 
 ifeq ($(BOARD),nucleo-f446re)
@@ -81,6 +89,8 @@ GPS_UART_DEV = 1
 GPS_RESET_PIN = GPIO_PIN\(PORT_B,10\)
 GPS_STANDBY_PIN = GPIO_PIN\(PORT_B,4\)
 GPS_UART_ENABLE_TRACE = 0
+# Enable GPS in SX1302 driver
+ENABLE_GPS = 1
 endif
 
 
@@ -98,17 +108,23 @@ GPS_UART_DEV = 1
 #        .tx_pin     = GPIO_PIN(PORT_A, 9), ARDUINO_D9
 GPS_RESET_PIN = GPIO_PIN\(PORT_B,10\)
 GPS_STANDBY_PIN = GPIO_PIN\(PORT_B,4\)
+# Enable GPS in SX1302 driver
+ENABLE_GPS = 1
 endif
 
 
 ifeq ($(BOARD),nucleo-l432kc-inisat)
 GPS_UART_DEV = 1
 GPS_BAUDRATE = 9600
+# Enable GPS in SX1302 driver
+ENABLE_GPS = 1
 endif
-
 
 ifeq ($(RAK5146_ON_NUCLEO32),yes)
 CFLAGS += -DRAK5146_ON_NUCLEO32=1
+# Enable GPS in SX1302 driver
+ENABLE_GPS = 1
+
 # No temperature Sensor on RAK5146
 # No PPS pin
 
@@ -122,6 +138,11 @@ endif
 ifdef GPS_UART_DEV
 CFLAGS += -DGPS_UART_DEV=$(GPS_UART_DEV)
 CFLAGS += -DGPS_UART_ENABLE_TRACE=$(GPS_UART_ENABLE_TRACE)
+endif
+
+ifdef ENABLE_GPS
+# Enable GPS in SX1302 driver
+CFLAGS += -DENABLE_GPS=$(ENABLE_GPS)
 endif
 
 ifdef GPS_MODEL
