ef9f2b82c3d331025cabae981ed5fe77f8944044
Didier DONSEZ <didier.donsez@gmail.com>
2026-07-08 10:25:14 +0200

    add headers for including all headers

---
+++ modules/meshcore_utils/include/meshcore_utils.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2026 Université Grenoble Alpes - CSUG - LIG
+ */
+
+/*
+ * Utils for MeshCore mesh network
+ */
+
+#ifndef _MESHCORE_UTILS_MESHCORE_H_
+#define _MESHCORE_UTILS_MESHCORE_H_
+
+// Import all
+
+#include "meshcore_constants.h"
+#include "meshcore_channel.h"
+#include "meshcore_contact.h"
+#include "meshcore_packet.h"
+#include "meshcore_payload.h"
+#include "meshcore_stats.h"
+
+#endif
