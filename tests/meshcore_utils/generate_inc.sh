d6e58b12901fe5ea6382bc3b03256d2197c01ae9
Didier DONSEZ <didier.donsez@gmail.com>
2026-07-08 12:44:52 +0200

    add todo

--- tests/meshcore_utils/generate_inc.sh
+++ tests/meshcore_utils/generate_inc.sh
@@ -1,14 +1,24 @@
 # Author: Didier DONSEZ
 
-echo Generating packets.inc for including packets
-
 # For generation time
 NOW=$(date)
+
+// ----------
+
+echo "TODO Generating channels.inc for populating known channels"
+
+{
+cat channels.csv
+}
+
+// ----------
+
+echo "Generating packets.inc for testing packets (including advert)"
+
 {
 
 echo "typedef struct packet_list { unsigned int len, unsigned char* packet} packet_list_t;" > .packets.inc
 echo "const packet_list_t p = {" >> .packets.inc
-
 
 LINECNT=0
 echo "/* Generated for at $NOW by $USER */"
@@ -32,3 +42,7 @@ cat .packets.inc >> packets.inc
 
 rm .packets.inc
 rm .packet.tmp
+
+// ----------
+
+echo TODO Generating unvalid_packets.inc for testing unvalid packets
